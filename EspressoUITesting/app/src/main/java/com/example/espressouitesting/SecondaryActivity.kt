555717c866415832dbf21b9fa33e581125a713d7
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-24 19:43:55 +0300

    Testing Activities in Isolation

---
+++ EspressoUITesting/app/src/main/java/com/example/espressouitesting/SecondaryActivity.kt
@@ -0,0 +1,17 @@
+package com.example.espressouitesting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_secondary.*
+
+class SecondaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary);
+
+        button_back.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
