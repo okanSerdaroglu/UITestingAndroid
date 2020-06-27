318f4adc7c24719bee7e29f329c8ec464f99ad7a
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-27 23:04:48 +0300

    Second Way of Creating a Custom Test Rule

--- EspressoUITesting/app/src/androidTest/java/com/example/espressouitesting/util/EspressoIdlingResourceRule.kt
+++ EspressoUITesting/app/src/androidTest/java/com/example/espressouitesting/util/EspressoIdlingResourceRule.kt
@@ -1,37 +1,60 @@
 package com.example.espressouitesting.util
 
 import androidx.test.espresso.IdlingRegistry
-import org.junit.rules.TestRule
+
+import org.junit.rules.TestWatcher
 import org.junit.runner.Description
-import org.junit.runners.model.Statement
-import kotlin.Exception
 
 /**
  * Option 1:
  * This option is much more difficult to read and is more verbose
  */
-
+/**
 class EspressoIdlingResourceRule : TestRule {
 
-    override fun apply(base: Statement?, description: Description?): Statement {
-        return IdlingResourceStatement(base)
+
+override fun apply(base: Statement?, description: Description?): Statement {
+return IdlingResourceStatement(base)
+}
+
+class IdlingResourceStatement(private val base: Statement?) : Statement() {
+
+private val idlingResource = EspressoIdlingResource.countingIdlingResource
+
+override fun evaluate() {
+// before
+IdlingRegistry.getInstance().register(idlingResource)
+try {
+base?.evaluate()
+?: throw Exception("Error evaluating test. Base statement is null.")
+} finally {
+// after
+IdlingRegistry.getInstance().unregister(idlingResource)
+}
+}
+}
+
+}*/
+
+
+/**
+ * option 2
+ * Simplified of option 1. (TestWatcher class implements TestRule)
+*/
+class EspressoIdlingResourceRule : TestWatcher() {
+
+    private val idlingResource = EspressoIdlingResource.countingIdlingResource
+
+    // Before
+    override fun starting(description: Description?) {
+        IdlingRegistry.getInstance().register(idlingResource)
+        super.starting(description)
     }
 
-    class IdlingResourceStatement(private val base: Statement?) : Statement() {
-
-        private val idlingResource = EspressoIdlingResource.countingIdlingResource
-
-        override fun evaluate() {
-            // before
-            IdlingRegistry.getInstance().register(idlingResource)
-            try {
-                base?.evaluate()
-                    ?: throw Exception("Error evaluating test. Base statement is null.")
-            } finally {
-                // after
-                IdlingRegistry.getInstance().unregister(idlingResource)
-            }
-        }
+    // After
+    override fun finished(description: Description?) {
+        IdlingRegistry.getInstance().unregister(idlingResource)
+        super.finished(description)
     }
 
 }
